4b2503da8753850110ca4aaf1b4c94422d3ead81
Donovan Schafroth <donovanschafroth@gmail.com>
2019-07-22 01:40:18 -0500

    test

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,42 +1,42 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyC58ZRYZVOIVT7avMPItHaFOB_pdAEZJJ8",
-    authDomain: "vegfinder-1ff47.firebaseapp.com",
-    databaseURL: "https://vegfinder-1ff47.firebaseio.com",
-    projectId: "vegfinder-1ff47",
-    storageBucket: "",
-    messagingSenderId: "817772042133",
-    appId: "1:817772042133:web:3aaa943319973970"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-
-
-//   Get Elements
-
-var txtEmail = document.getElementById("txtEmail");
-var txtPassword = document.getElementById("txtPassword");
-var btnLogin = document.getElementById("btnLogin");
-var btnSignUp = document.getElementById("btnSignUp");
-var btnLogout = document.getElementById("btnLogout");
-
-// Add Login Event
-btnLogin.addEventListener('click', e => {
-    var email = txtEmail.value;
-    var password = txtPassword.value;
-    var auth = firebase.auth();
-
-    // Sign in
-    var promise = auth.signInWithEmailAndPassword(email, pass)
-    promise.catch(e => console.log(e.message)); 
-    
-});
-
 
 $(document).ready(function () {
     console.log("Page loaded!")
 
-
+    var firebaseConfig = {
+        apiKey: "AIzaSyC58ZRYZVOIVT7avMPItHaFOB_pdAEZJJ8",
+        authDomain: "vegfinder-1ff47.firebaseapp.com",
+        databaseURL: "https://vegfinder-1ff47.firebaseio.com",
+        projectId: "vegfinder-1ff47",
+        storageBucket: "",
+        messagingSenderId: "817772042133",
+        appId: "1:817772042133:web:3aaa943319973970"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    
+    
+    
+    //   Get Elements
+    
+    var txtEmail = document.getElementById("txtEmail");
+    var txtPassword = document.getElementById("txtPassword");
+    var btnLogin = document.getElementById("btnLogin");
+    var btnSignUp = document.getElementById("btnSignUp");
+    var btnLogout = document.getElementById("btnLogout");
+    
+    // Add Login Event
+    btnLogin.addEventListener('click', e => {
+        var email = txtEmail.value;
+        var password = txtPassword.value;
+        var auth = firebase.auth();
+    
+        // Sign in
+        var promise = auth.signInWithEmailAndPassword(email, pass)
+        promise.catch(e => console.log(e.message)); 
+        
+    });
+    
+    
     function populateCards() {
 
 
